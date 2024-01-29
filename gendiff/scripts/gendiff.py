fd4494115ce0346b193d672aac1571c08137c54c
Rem <morrig407@gmail.com>
2024-01-29 18:12:07 +0700

    Add parse_file(); add example/ files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,12 @@
 import argparse
+import json
+
+
+def parse_file(path):
+    if not path:
+        raise ValueError("Path name cannot be empty!")
+    lines = json.load(open(path))
+    return lines
 
 
 def main():
@@ -8,7 +16,13 @@ def main():
     parser.add_argument("-f", "--format", help="set format of output")
     parser.add_argument("first_file", type=str)
     parser.add_argument("second_file", type=str)
-    parser.parse_args()
+    args = parser.parse_args()
+
+    file1 = parse_file(args.first_file)
+    file2 = parse_file(args.second_file)
+
+    print(set(file1) & set(file2))
+    print(set(file1) ^ set(file2))
 
 
 if __name__ == "__main__":
