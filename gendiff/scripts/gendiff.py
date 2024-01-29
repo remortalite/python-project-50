8652aa5b1fe19be4949c1009d3eb4df819a88f00
Rem <morrig407@gmail.com>
2024-01-29 18:24:27 +0700

    Sep arg_parse() at utils/

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,8 @@
-import argparse
-
-from gendiff.utils import parse_file
+from gendiff.utils import parse_file, parse_args
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
-    )
-    parser.add_argument("-f", "--format", help="set format of output")
-    parser.add_argument("first_file", type=str)
-    parser.add_argument("second_file", type=str)
-    args = parser.parse_args()
+    args = parse_args()
 
     file1 = parse_file(args.first_file)
     file2 = parse_file(args.second_file)
--- gendiff/utils/__init__.py
+++ gendiff/utils/__init__.py
@@ -1,1 +1,5 @@
 from gendiff.utils.parse_file import parse_file
+from gendiff.utils.cli import parse_args
+
+__all__ = (parse_file,
+           parse_args)
