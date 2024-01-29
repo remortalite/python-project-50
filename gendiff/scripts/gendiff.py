22972990fc184847e3658ef0d6b98a34fbf8d27e
Rem <morrig407@gmail.com>
2024-01-29 21:26:53 +0700

    Add global prefixes

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,11 +2,16 @@ import json
 import argparse
 
 
+PREFIX_IF_FIRST = "- "
+PREFIX_IF_SECOND = "+ "
+PREFIX_IF_SAME = "  "
+
+
 def add_prefix(name, prefix):
     return prefix + str(name)
 
 
-def add_prefix_to_dict_keys(items, prefix="  "):
+def add_prefix_to_dict_keys(items, prefix=PREFIX_IF_SAME):
     new_dict = {}
     for (key, value) in items.items():
         new_dict[add_prefix(key, prefix)] = value
@@ -15,12 +20,16 @@ def add_prefix_to_dict_keys(items, prefix="  "):
 
 def diff_item_sort(item):
     tuple_ = (item[0][2:],
-              0 if item[0] == '-' else 1)
+              0 if item[0] == PREFIX_IF_FIRST[0] else 1)
     return tuple_
 
 
 def get_dict_sorted(items):
     return dict(sorted(items.items(), key=diff_item_sort))
+
+
+def filter_dict(items, keys_include):
+    return {k: v for (k, v) in items.items() if k in keys_include}
 
 
 def generate_diff(file_path1, file_path2):
@@ -31,19 +40,19 @@ def generate_diff(file_path1, file_path2):
     diff_keys_first = set(file1) - set(file2)
     diff_keys_second = set(file2) - set(file1)
 
-    diff_first = add_prefix_to_dict_keys({k: v for (k, v) in file1.items()
-                                          if k in diff_keys_first}, "- ")
-    diff_second = add_prefix_to_dict_keys({k: v for (k, v) in file2.items()
-                                          if k in diff_keys_second}, "+ ")
+    diff_first = add_prefix_to_dict_keys(filter_dict(file1, diff_keys_first),
+                                         PREFIX_IF_FIRST)
+    diff_second = add_prefix_to_dict_keys(filter_dict(file2, diff_keys_second),
+                                          PREFIX_IF_SECOND)
 
     result_dict = diff_first | diff_second
 
     for key in common_keys:
         if file1[key] == file2[key]:
-            result_dict[add_prefix(key, "  ")] = file1[key]
+            result_dict[add_prefix(key, PREFIX_IF_SAME)] = file1[key]
         else:
-            result_dict[add_prefix(key, "- ")] = file1[key]
-            result_dict[add_prefix(key, "+ ")] = file2[key]
+            result_dict[add_prefix(key, PREFIX_IF_FIRST)] = file1[key]
+            result_dict[add_prefix(key, PREFIX_IF_SECOND)] = file2[key]
 
     result_dict = get_dict_sorted(result_dict)
     print(json.dumps(result_dict, indent=4).replace('"', ''))
